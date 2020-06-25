cc0508ed193009bf612f1313608c5ea90261e69f
Kalen Goo <44537937+kgoo124@users.noreply.github.com>
2020-06-24 16:13:38 -1000

    Extension renders an EditableDataGrid (#12)
    * "WIP EditableCSVViewer"
    
    * Datamodel experiment (#9)
    
    * Created an EditableDataModel class
    
    * Created a template for some of the EditableDataModel methods
    
    * Extension now renders an EditableDataGrid instead of a DataGrid
    
    Co-authored-by: Logan McNichols <loganamcnichols@gmail.com>

--- src/editabledatamodel.ts
+++ src/editabledatamodel.ts
@@ -1,47 +1,40 @@
-import { MutableDataModel, DataModel } from '@lumino/datagrid';
+// import { MutableDataModel, DataModel } from '@lumino/datagrid';
 
-export default class EditableDataModel extends MutableDataModel {
+// export default class EditableDataModel extends MutableDataModel {
+//   rowCount(region: DataModel.RowRegion): number {
+//     return 0;
+//   }
 
-    rowCount(region: DataModel.RowRegion): number {
-        return 0
-    }
+//   columnCount(region: DataModel.ColumnRegion): number {
+//     return 0;
+//   }
 
-    columnCount(region: DataModel.ColumnRegion): number {
-          return 0
-    }
+//   data(): void {}
 
-    data(): void { }
+//   //   @param region - The cell region of interest.
+//   //   @param row - The row index of the cell of interest.
+//   //   @param column - The column index of the cell of interest.
+//   //
+//   //
+//   //     @returns true if succeeds, false otherwise.
 
+//   setData(): boolean {
+//     return false;
+//   }
 
-   
+//   //   @param position - the column index of the selected column.
 
+//   addColumn(row: number, column: number): void {
+//     let colCount = this.columnCount('body');
+//     // TODO: how do we get the position of the highlighted cell.
+//     ++colCount;
+//     // Todo: what do we return?
+//   }
+//   // @param  position - the row index of the selected row
 
-//   @param region - The cell region of interest.
-//   @param row - The row index of the cell of interest.
-//   @param column - The column index of the cell of interest.
-//    
-//    
-//     @returns true if succeeds, false otherwise.
-
-setData(): boolean { return false }
-
-
-//   @param position - the column index of the selected column.
-
-
-addColumn(row: number, column: number): void {
-    let colCount = this.columnCount('body');
-    // TODO: how do we get the position of the highlighted cell.
-    ++ colCount;
-    // Todo: what do we return?
-}
-// @param  position - the row index of the selected row
-
-addRow(row: number, column: number): void {
-    let rowCount = this.rowCount('body');
-    ++ rowCount;
-    // TODO: what do we return?
-
-}
-
-}
+//   addRow(row: number, column: number): void {
+//     let rowCount = this.rowCount('body');
+//     ++rowCount;
+//     // TODO: what do we return?
+//   }
+// }
