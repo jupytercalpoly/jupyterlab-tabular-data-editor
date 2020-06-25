6031b19e60f4ce3b7fd3880bdb40e82f41d3edaa
Logan McNichols <loganamcnichols@gmail.com>
2020-06-25 16:14:22 -0700

    Got extension to build

--- src/editabledatamodel.ts
+++ src/editabledatamodel.ts
@@ -38,62 +38,63 @@ export default class EditableDataModel extends DSVModel {
         // this might do it
         this._parseAsync();
     }
-    /**
-     * Notes: I am note sure if the way I have initially setup this function is too restrictive by
-     * assuming we are only editing one cell at a time.
-     * 
-     * @param row -index of the row in which we are making a change
-     * @param column -index of the column in which we are making a change
-     */
-    changeCell(row: number, column: number) {
-        // index of the first char of entry in question
-        const index: number = this._getOffsetIndex(row, column);
-
-        // find the index to the first char of the next entry.
-        let nextIndex: number;
-        let trimRight = 0;
-        let trimLeft = 0;
-        // Find the end of the slice (the start of the next field), and how much we
-        // should adjust to trim off a trailing field or row delimiter. First check
-        // if we are getting the last column.
-        if (column === this._columnCount - 1) {
-            // Check if we are getting any row but the last.
-            if (row < this._rowCount - 1) {
-                // Set the next offset to the next row, column 0.
-                nextIndex = this._getOffsetIndex(row + 1, 0);
-                // Since we are not at the last row, we need to trim off the row
-                // delimiter.
-                trimRight += this._rowDelimiter.length;
-            }
-            else {
-                // We are getting the last data item, so the slice end is the end of the
-                // data string.
-                nextIndex = this._data.length;
-                // The string may or may not end in a row delimiter (RFC 4180 2.2), so
-                // we explicitly check if we should trim off a row delimiter.
-                if (this._data[nextIndex - 1] ===
-                    this._rowDelimiter[this._rowDelimiter.length - 1]) {
-                    trimRight += this._rowDelimiter.length;
-                }
-            }
-        }
-        else {
-            // The next field starts at the next column offset.
-            nextIndex = this._getOffsetIndex(row, column + 1);
-            // Trim off the delimiter if it exists at the end of the field
-            if (index < nextIndex && this._data[nextIndex - 1] === this._delimiter) {
-                trimRight += 1;
-            }
-        }
-        // Check to see if the field begins with a quote. If it does, trim a quote on either side.
-        if (this._data[index] === this._quote) {
-            trimLeft += 1;
-            trimRight += 1;
-        }
-        // const lastCharIndex: number = nextIndex - trimRight;
-        // const firstCharIndex: number = index + trimLeft;
-        
-    }
-
-
 }
+//     /**
+//      * Notes: I am note sure if the way I have initially setup this function is too restrictive by
+//      * assuming we are only editing one cell at a time.
+//      * 
+//      * @param row -index of the row in which we are making a change
+//      * @param column -index of the column in which we are making a change
+//      */
+//     changeCell(row: number, column: number) {
+//         // index of the first char of entry in question
+//         const index: number = this._getOffsetIndex(row, column);
+
+//         // find the index to the first char of the next entry.
+//         let nextIndex: number;
+//         let trimRight = 0;
+//         let trimLeft = 0;
+//         // Find the end of the slice (the start of the next field), and how much we
+//         // should adjust to trim off a trailing field or row delimiter. First check
+//         // if we are getting the last column.
+//         if (column === this._columnCount - 1) {
+//             // Check if we are getting any row but the last.
+//             if (row < this._rowCount - 1) {
+//                 // Set the next offset to the next row, column 0.
+//                 nextIndex = this._getOffsetIndex(row + 1, 0);
+//                 // Since we are not at the last row, we need to trim off the row
+//                 // delimiter.
+//                 trimRight += this._rowDelimiter.length;
+//             }
+//             else {
+//                 // We are getting the last data item, so the slice end is the end of the
+//                 // data string.
+//                 nextIndex = this._data.length;
+//                 // The string may or may not end in a row delimiter (RFC 4180 2.2), so
+//                 // we explicitly check if we should trim off a row delimiter.
+//                 if (this._data[nextIndex - 1] ===
+//                     this._rowDelimiter[this._rowDelimiter.length - 1]) {
+//                     trimRight += this._rowDelimiter.length;
+//                 }
+//             }
+//         }
+//         else {
+//             // The next field starts at the next column offset.
+//             nextIndex = this._getOffsetIndex(row, column + 1);
+//             // Trim off the delimiter if it exists at the end of the field
+//             if (index < nextIndex && this._data[nextIndex - 1] === this._delimiter) {
+//                 trimRight += 1;
+//             }
+//         }
+//         // Check to see if the field begins with a quote. If it does, trim a quote on either side.
+//         if (this._data[index] === this._quote) {
+//             trimLeft += 1;
+//             trimRight += 1;
+//         }
+//         // const lastCharIndex: number = nextIndex - trimRight;
+//         // const firstCharIndex: number = index + trimLeft;
+        
+//     }
+
+
+// }
