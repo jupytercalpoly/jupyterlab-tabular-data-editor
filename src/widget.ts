17372e546712b07bde7eda3a64a7f691eea37893
Logan McNichols <loganamcnichols@gmail.com>
2020-06-29 15:02:28 -0700

    Got a working version of the widget.ts module. CSVWidget does not appear to be set up in a way that we can easily extend it, so I copied the source code and modified some of the attributes and functions to point to our custom model and grid

--- src/widget.ts
+++ src/widget.ts
@@ -1,20 +1,47 @@
-import { CSVViewer, 
-  CSVDocumentWidget,
-  CSVViewerFactory,
-   GridSearchService } from '@jupyterlab/csvviewer';
-import { DocumentRegistry, IDocumentWidget, DocumentWidget } from '@jupyterlab/docregistry';
-import EditableDataGrid from './grid';
-import { BasicKeyHandler, BasicMouseHandler, DataGrid } from '@lumino/datagrid';
-import { PanelLayout } from '@lumino/widgets';
 import { ActivityMonitor } from '@jupyterlab/coreutils';
 
+import {
+  ABCWidgetFactory,
+  DocumentRegistry,
+  IDocumentWidget,
+  DocumentWidget
+} from '@jupyterlab/docregistry';
 
+import { PromiseDelegate } from '@lumino/coreutils';
+import { CSVViewer, GridSearchService, TextRenderConfig,  } from '@jupyterlab/csvviewer';
+
+import {
+  BasicKeyHandler,
+  BasicMouseHandler,
+  BasicSelectionModel,
+  DataGrid,
+  TextRenderer
+} from '@lumino/datagrid';
+
+import { Message } from '@lumino/messaging';
+
+import { PanelLayout, Widget } from '@lumino/widgets';
+import EditableDataGrid from './grid'
+import EditableDSVModel from './model'
+
+
+const CSV_CLASS = 'jp-CSVViewer';
 const CSV_GRID_CLASS = 'jp-CSVViewer-grid';
 const RENDER_TIMEOUT = 1000;
 
-export class EditableCSVViewer extends CSVViewer {
+
+export class EditableCSVViewer extends Widget {
+  /**
+   * Construct a new CSV viewer.
+   */
   constructor(options: CSVViewer.IOptions) {
-    super(options);
+    super();
+
+    const context = (this._context = options.context);
+    const layout = (this.layout = new PanelLayout());
+
+    this.addClass(CSV_CLASS);
+
     this._grid = new EditableDataGrid({
       defaultSizes: {
         rowHeight: 24,
@@ -23,8 +50,6 @@ export class EditableCSVViewer extends CSVViewer {
         columnHeaderHeight: 36
       }
     });
-    const layout = (this.layout = new PanelLayout());
-    const context = (this._context = options.context);
     this._grid.addClass(CSV_GRID_CLASS);
     this._grid.headerVisibility = 'all';
     this._grid.keyHandler = new BasicKeyHandler();
@@ -35,6 +60,7 @@ export class EditableCSVViewer extends CSVViewer {
       headers: 'all',
       warningThreshold: 1e6
     };
+
     layout.addWidget(this._grid);
 
     this._searchService = new GridSearchService(this._grid);
@@ -51,19 +77,158 @@ export class EditableCSVViewer extends CSVViewer {
       this._monitor.activityStopped.connect(this._updateGrid, this);
     });
   }
+
+  /**
+   * The CSV widget's context.
+   */
+  get context(): DocumentRegistry.Context {
+    return this._context;
+  }
+
+  /**
+   * A promise that resolves when the csv viewer is ready to be revealed.
+   */
+  get revealed() {
+    return this._revealed.promise;
+  }
+
+  /**
+   * The delimiter for the file.
+   */
+  get delimiter(): string {
+    return this._delimiter;
+  }
+  set delimiter(value: string) {
+    if (value === this._delimiter) {
+      return;
+    }
+    this._delimiter = value;
+    this._updateGrid();
+  }
+
+  /**
+   * The style used by the data grid.
+   */
+  get style(): DataGrid.Style {
+    return this._grid.style;
+  }
+  set style(value: DataGrid.Style) {
+    this._grid.style = value;
+  }
+
+  /**
+   * The config used to create text renderer.
+   */
+  set rendererConfig(rendererConfig: TextRenderConfig) {
+    this._baseRenderer = rendererConfig;
+    this._updateRenderer();
+  }
+
+  /**
+   * The search service
+   */
+  get searchService(): GridSearchService {
+    return this._searchService;
+  }
+
+  /**
+   * Dispose of the resources used by the widget.
+   */
+  dispose(): void {
+    if (this._monitor) {
+      this._monitor.dispose();
+    }
+    super.dispose();
+  }
+
+  /**
+   * Go to line
+   */
+  goToLine(lineNumber: number) {
+    this._grid.scrollToRow(lineNumber);
+  }
+
+  /**
+   * Handle `'activate-request'` messages.
+   */
+  protected onActivateRequest(msg: Message): void {
+    this.node.tabIndex = -1;
+    this.node.focus();
+  }
+
+  /**
+   * Create the model for the grid.
+   */
+  protected _updateGrid() {
+    const data = this._context.model.toString();
+    const delimiter = ',';
+    const oldModel = this._grid.dataModel as EditableDSVModel;
+    const dataModel = (this._grid.dataModel = new EditableDSVModel({
+      data,
+      delimiter
+    }));
+    this._grid.selectionModel = new BasicSelectionModel({ dataModel });
+    if (oldModel) {
+      oldModel.dispose();
+    }
+  }
+
+  /**
+   * Update the renderer for the grid.
+   */
+  private _updateRenderer(): void {
+    if (this._baseRenderer === null) {
+      return;
+    }
+    const rendererConfig = this._baseRenderer;
+    const renderer = new TextRenderer({
+      textColor: rendererConfig.textColor,
+      horizontalAlignment: rendererConfig.horizontalAlignment,
+      backgroundColor: this._searchService.cellBackgroundColorRendererFunc(
+        rendererConfig
+      )
+    });
+    this._grid.cellRenderers.update({
+      body: renderer,
+      'column-header': renderer,
+      'corner-header': renderer,
+      'row-header': renderer
+    });
+  }
+
+  private _context: DocumentRegistry.Context;
+  private _grid: DataGrid;
+  private _searchService: GridSearchService;
+  private _monitor: ActivityMonitor<
+    DocumentRegistry.IModel,
+    void
+  > | null = null;
+  private _delimiter = ',';
+  private _revealed = new PromiseDelegate<void>();
+  private _baseRenderer: TextRenderConfig | null = null;
 }
 
-export class EditableCSVDocumentWidget extends DocumentWidget<CSVViewer> { 
-  constructor(options: CSVDocumentWidget.IOptions) {
+
+  // Override the CSVViewer's _updateGrid method to set the datagrid's model to an EditableDataModel
+
+  
+
+export class EditableCSVDocumentWidget extends DocumentWidget<EditableCSVViewer> {
+  constructor(options: EditableCSVDocumentWidget.IOptions) {
     let { context, content, reveal } = options;
     content = content || new EditableCSVViewer({ context });
     reveal = Promise.all([reveal, content.revealed]);
-    super(Object.assign({context, content, reveal}));
+    super(Object.assign({ context, content, reveal }));
+  }
+}
+export declare namespace EditableCSVDocumentWidget {
+  interface IOptions extends DocumentWidget.IOptionsOptionalContent<EditableCSVViewer> {
+      delimiter?: string;
   }
 }
 
-export class EditableCSVViewerFactory extends CSVViewerFactory {
-  createNewWidget(context: DocumentRegistry.Context): IDocumentWidget<CSVViewer> {
+export class EditableCSVViewerFactory extends ABCWidgetFactory<IDocumentWidget<EditableCSVViewer>> {
+  createNewWidget(context: DocumentRegistry.Context): IDocumentWidget<EditableCSVViewer> {
     return new EditableCSVDocumentWidget({ context })
   }
 }
