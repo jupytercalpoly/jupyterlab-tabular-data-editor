e0ea321ff1195315184aaf2eb63d89851a21a912
Logan McNichols <loganamcnichols@gmail.com>
2020-07-02 12:44:24 -0700

    Modified names of DSV attributes/methods to follow Jupyterlab's standard naming convention for public attributes/methods. Used getters for columnCount and rowCount in place of direct usage of _columnCount and _rowCount attributes

--- src/model.ts
+++ src/model.ts
@@ -46,7 +46,7 @@ export default class EditableDSVModel extends MutableDataModel {
     // Look up the field and value for the region.
     switch (region) {
       case 'body':
-        if (model._header.length === 0) {
+        if (model.header.length === 0) {
           this._setField(row, column, value);
         } else {
           this._setField(row + 1, column, value);
@@ -94,7 +94,7 @@ export default class EditableDSVModel extends MutableDataModel {
     let nextIndex;
 
     // Find the index for the first character in the field.
-    const index = model._getOffsetIndex(row, column);
+    const index = model.getOffsetIndex(row, column);
 
     // Initialize the trim adjustments.
     let trimRight = 0;
@@ -103,53 +103,53 @@ export default class EditableDSVModel extends MutableDataModel {
     // Find the end of the slice (the start of the next field), and how much we
     // should adjust to trim off a trailing field or row delimiter. First check
     // if we are getting the last column.
-    if (column === model._columnCount! - 1) {
+    if (column === model.columnCount('body')! - 1) {
       // Check if we are getting any row but the last.
-      if (row < model._rowCount! - 1) {
+      if (row < model.rowCount('body')! - 1) {
         // Set the next offset to the next row, column 0.
-        nextIndex = model._getOffsetIndex(row + 1, 0);
+        nextIndex = model.getOffsetIndex(row + 1, 0);
 
         // Since we are not at the last row, we need to trim off the row
         // delimiter.
-        trimRight += model._rowDelimiter.length;
+        trimRight += model.rowDelimiter.length;
       } else {
         // We are getting the last data item, so the slice end is the end of the
         // data string.
-        nextIndex = model._data.length;
+        nextIndex = model.rawData.length;
 
         // The string may or may not end in a row delimiter (RFC 4180 2.2), so
         // we explicitly check if we should trim off a row delimiter.
         if (
-          model._data[nextIndex - 1] ===
-          model._rowDelimiter[model._rowDelimiter.length - 1]
+          model.rawData[nextIndex - 1] ===
+          model.rowDelimiter[model.rowDelimiter.length - 1]
         ) {
-          trimRight += model._rowDelimiter.length;
+          trimRight += model.rowDelimiter.length;
         }
       }
     } else {
       // The next field starts at the next column offset.
-      nextIndex = model._getOffsetIndex(row, column + 1);
+      nextIndex = model.getOffsetIndex(row, column + 1);
 
       // Trim off the delimiter if it exists at the end of the field
       if (
         index < nextIndex &&
-        model._data[nextIndex - 1] === model._delimiter
+        model.rawData[nextIndex - 1] === model.delimiter
       ) {
         trimRight += 1;
       }
     }
 
     // Check to see if the field begins with a quote. If it does, trim a quote on either side.
-    if (model._data[index] === model._quote) {
+    if (model.rawData[index] === model.quote) {
       trimLeft += 1;
       trimRight += 1;
     }
-    model._data =
-      model._data.slice(0, index + trimLeft) +
+    model.rawData =
+      model.rawData.slice(0, index + trimLeft) +
       value +
-      model._data.slice(nextIndex - trimRight, model._data.length);
+      model.rawData.slice(nextIndex - trimRight, model.rawData.length);
 
-    model._parseAsync();
+    model.parseAsync();
   }
 
   private _dsvModel: DSVModel;
