99a2530c6d1f9455414bb79a07662f7a364f354d
Kalen Goo <ktwmgoo@gmail.com>
2020-07-05 12:25:07 -1000

    Defined return types for handler.ts functions

--- src/handler.ts
+++ src/handler.ts
@@ -2,16 +2,17 @@ import { BasicMouseHandler, DataGrid } from '@lumino/datagrid';
 import { Signal } from '@lumino/signaling';
 
 export default class RichMouseHandler extends BasicMouseHandler {
-    onContextMenu(grid: DataGrid, event: MouseEvent) {
-        const {clientX, clientY } = event;
-        let hit = grid.hitTest(clientX, clientY);
-        const { row, column } = hit;
-        console.log(row, column)
-        this._rightClickSignal.emit([row, column])
-    }
-    get rightClickSignal() {
-        return this._rightClickSignal
-    }
+  onContextMenu(grid: DataGrid, event: MouseEvent): void {
+    const { clientX, clientY } = event;
+    const hit = grid.hitTest(clientX, clientY);
+    const { row, column } = hit;
+    console.log(row, column);
+    this._rightClickSignal.emit([row, column]);
+  }
 
-    private _rightClickSignal = new Signal<this, Array<number>>(this)
+  get rightClickSignal(): Signal<this, Array<number>> {
+    return this._rightClickSignal;
+  }
+
+  private _rightClickSignal = new Signal<this, Array<number>>(this);
 }
