473bf3be502e4f79cd50b6696eb8c608d2cee980
Logan McNichols <loganamcnichols@gmail.com>
2020-07-07 17:46:20 -0700

    Fixed bug with the column function

--- src/model.ts
+++ src/model.ts
@@ -204,19 +204,50 @@ export default class EditableDSVModel extends MutableDataModel {
 
   removeCol(colNumber: number): void {
     const model = this.dsvModel;
-    // this feels sub-optimal but I haven't thought of a better way.
+
     let startIndex: number;
     let endIndex: number;
+    //records length of the data we just removed
     let diff: number;
+    // the row we are processing
+    let currentRow: number;
+    // total data we removed
+    // the row of the next entry (or we're at the end)
+    let nextRow: number;
     let shift = 0;
-    for (let row = 0; row <= model.rowCount('body'); row++) {
-      startIndex = model.getOffsetIndex(row, colNumber) - shift;
-      endIndex = model.getOffsetIndex(row, colNumber + 1) - shift;
+    // Accounts for length of trailing delimeter if we are removing last column
+    let trailingDelimeter = 0;
+    // Accounts for length of trailing rowDelimeter if we are removing last column
+    let trailingRowDelimeter = 0;
+    // The column of the next entry. 0 if we are removing the last column.
+    let nextCol = colNumber + 1;
+    // If we are removing last column, next entry is on next row, so this gets set to 1.
+    let rowShift = 0;
+    // check if we are removing last column
+    if (colNumber + 1 === model.columnCount('body')) {
+      trailingDelimeter = model.delimiter.length;
+      trailingRowDelimeter = model.rowDelimiter.length;
+      nextCol = 0;
+      rowShift = 1;
+    }
+
+    for (currentRow = 0; currentRow <= model.rowCount('body'); currentRow++) {
+      nextRow = currentRow + rowShift;
+      startIndex =
+        model.getOffsetIndex(currentRow, colNumber) - shift - trailingDelimeter;
+      if (nextRow > this.rowCount('body')) {
+        endIndex = model.rawData.length;
+      } else {
+        endIndex =
+          model.getOffsetIndex(nextRow, nextCol) - shift - trailingRowDelimeter;
+      }
       diff = endIndex - startIndex;
       model.rawData =
         model.rawData.slice(0, startIndex) + model.rawData.slice(endIndex);
       shift += diff;
+      console.log(model.rawData);
     }
+
     console.log(model.rawData);
     model.parseAsync();
     this.emitChanged({
