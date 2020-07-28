a062fac4913350257f0ac73952b8cfff52a9efd1
Logan McNichols <loganamcnichols@gmail.com>
2020-07-28 16:55:32 -0700

    context menu ready to go

--- src/selection.ts
+++ src/selection.ts
@@ -1,5 +1,5 @@
 import { h, VirtualDOM } from '@lumino/virtualdom';
-import { Drag } from '@lumino/dragdrop';
+import { Drag } from 'tde-dragdrop';
 import { MimeData } from '@lumino/coreutils';
 
 // Class names for the shadow/line
