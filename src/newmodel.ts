becfed253532ec186fc5eba8126bcbe268344c09
Logan McNichols <loganamcnichols@gmail.com>
2020-07-30 10:10:30 -0700

    all function frameworks written

--- src/newmodel.ts
+++ src/newmodel.ts
@@ -1,15 +1,19 @@
 import { DSVModel } from 'tde-csvviewer';
 import { MutableDataModel, DataModel } from '@lumino/datagrid';
+import { Fields } from '@lumino/datastore';
+// import { Litestore } from './litestore';
 
 export default class EditorModel extends MutableDataModel {
   private _model: DSVModel;
-  private _valueMap: Map<Array<number>, string>;
+  private _valueMap: HashMap;
   private _rowMap: Array<number>;
   private _columnMap: Array<number>;
   private _newRow: number;
   private _newColumn: number;
   private _initRows: number;
   private _initColumns: number;
+  private _clipboard: Array<Array<string>>;
+  // private _litestore: Litestore;
   constructor(options: DSVModel.IOptions) {
     super();
     // give our model the DSVModel as a property
@@ -36,7 +40,10 @@ export default class EditorModel extends MutableDataModel {
     // dataset. It maps [row, column] pairs to values and is
     // the first thing that is checked when a cell is queried
     // for its value.
-    this._valueMap = new Map();
+    this._valueMap = {};
+
+    // initialize the litestore
+    // this._litestore = new Litestore({ id: 0, schemas: [DATAMODEL_SCHEMA] });
   }
 
   /**
@@ -59,14 +66,13 @@ export default class EditorModel extends MutableDataModel {
     // row a unique id by indexing the first body row at 1
     row = this._uniqueRowID(row, region);
 
-    // map the requested row, column to where the data has
-    // been moved to through previous mutations
+    // map the Grid row/column to the static row/column where the data lives
     row = this._rowMap[row];
     column = this._columnMap[column];
 
     // check if a new value has been stored at this cell
-    if (this._valueMap.has([row, column])) {
-      return this._valueMap.get([row, column]);
+    if (this._valueMap[`${row}, ${column}`]) {
+      return this._valueMap[`${row}, ${column}`];
     }
 
     // do a bounds check to see if either the row or column is
@@ -97,7 +103,7 @@ export default class EditorModel extends MutableDataModel {
     column = this._columnMap[column];
 
     // add the value to the valueMap
-    this._valueMap.set([row, column], value);
+    this._valueMap[`${row}, ${column}`] = value;
 
     return true;
   }
@@ -108,7 +114,9 @@ export default class EditorModel extends MutableDataModel {
    *
    * Notes: this method (and all others that follow it)
    */
-  addRows(start: number, span = 1): void {
+  addRows(region: DataModel.CellRegion, start: number, span = 1): void {
+    // Map to the unique row ID.
+    start = this._uniqueRowID(start, region);
     // store the next span's worth of values
     const values = [];
     let i = 0;
@@ -127,7 +135,7 @@ export default class EditorModel extends MutableDataModel {
    * @param start the index at which to start adding columns.
    * @param span the number of columns to add. Default is 1.
    */
-  addColumns(start: number, span = 1): void {
+  addColumns(region: DataModel.CellRegion, start: number, span = 1): void {
     // store the next span's worth of values
     const values = [];
     let i = 0;
@@ -146,7 +154,9 @@ export default class EditorModel extends MutableDataModel {
    * @param start the index to start removing the rows
    * @param span the number of rows to remove
    */
-  removeRows(start: number, span: number): void {
+  removeRows(region: DataModel.CellRegion, start: number, span: number): void {
+    // map to the unique row ID.
+    start = this._uniqueRowID(start, region);
     // remove the values from the rowMap
     this._rowMap.splice(start, span);
   }
@@ -169,7 +179,16 @@ export default class EditorModel extends MutableDataModel {
    * @param span the number of columns moving
    */
 
-  moveRows(start: number, end: number, span: number): void {
+  moveRows(
+    region: DataModel.CellRegion,
+    start: number,
+    end: number,
+    span: number
+  ): void {
+    // Get the unique row ID.
+    start = this._uniqueRowID(start, region);
+    end = this._uniqueRowID(end, region);
+
     // bail early if we are moving no distance
     if (start === end) {
       return;
@@ -206,7 +225,12 @@ export default class EditorModel extends MutableDataModel {
    * @param end the index to insert the first column
    * @param span the number of columns moving
    */
-  moveColumns(start: number, end: number, span: number): void {
+  moveColumns(
+    region: DataModel.CellRegion,
+    start: number,
+    end: number,
+    span: number
+  ): void {
     // bail early if we are moving no distance
     if (start === end) {
       return;
@@ -238,25 +262,73 @@ export default class EditorModel extends MutableDataModel {
     this._columnMap.splice(destination, 0, ...valuesMoving);
   }
 
-  cut(row: number, column: number, rowSpan: number, columnSpan: number): void {
-    // we are redefining data in virtual space, so we must use the row and column
-    // maps to get the virtual row and column.
+  cut(
+    region: DataModel.CellRegion,
+    row: number,
+    column: number,
+    rowSpan: number,
+    columnSpan: number
+  ): void {
+    // we use the value map to redefine values within the cut as ''. Need to map
+    // to the static values.
+    // clear previous values from the clipboard
+    this._clipboard = [];
+    for (let i = rowSpan - 1; i >= 0; i--) {
+      const rowClip = [];
+      for (let j = columnSpan - 1; j >= 0; j--) {
+        // make a temporary copy of the values
+        rowClip.push(this.data(region, row + i, column + j));
+        this.setData(region, row, column, '');
+      }
+      this._clipboard.push(rowClip);
+    }
+  }
+  copy(
+    region: DataModel.CellRegion,
+    row: number,
+    column: number,
+    rowSpan: number,
+    columnSpan: number
+  ): void {
+    // we use the value map to redefine values within the cut as ''. Need to map
+    // to the static values.
+    // clear previous values from the clipboard
+    this._clipboard = [];
+    for (let i = rowSpan - 1; i >= 0; i--) {
+      const rowClip = [];
+      for (let j = columnSpan - 1; j >= 0; j--) {
+        // make a temporary copy of the values
+        rowClip.push(this.data(region, row + i, column + j));
+      }
+      this._clipboard.push(rowClip);
+    }
+  }
+
+  paste(region: DataModel.CellRegion, row: number, column: number): void {
+    // get the unique row ID.
+    row = this._uniqueRowID(row, region);
+    // see how much space we have
+    const rowsBelow = this.numRows - row;
+    const columnsRight = this.numColumns - column;
+
+    // clamp the values we are adding at the bounds of the grid
+    const rowSpan = Math.min(rowsBelow, this._clipboard.length);
+    const columnSpan = Math.min(columnsRight, this._clipboard[0].length);
+
+    // revert to grid's row id for setting the data
+    row = this._gridRowID(row, region);
+
+    // set the data
+    // TODO see if calling set data in for loop is bad for performance.
     for (let i = 0; i < rowSpan; i++) {
       for (let j = 0; j < columnSpan; j++) {
-        this._valueMap.set(
-          [this._rowMap[row + i], this._columnMap[column + j]],
-          ''
-        );
+        this.setData(region, row, column, this._clipboard[i][j]);
       }
     }
   }
-  // copy(row: number, column: number, rowSpan: number, columnSpan: number): void {
-  // }
-
-  // paste(row: number, column: number, rowSpan: number, columnSpan: number): void {
-  // }
 
   // undo(): void {
+
   // }
 
   // redo(): void {
@@ -308,3 +380,35 @@ export default class EditorModel extends MutableDataModel {
     return region === 'body' ? row - 1 : row;
   }
 }
+//   private _updateLiteStore(change: DataModel.ChangedArgs | null) {
+//     this._litestore.beginTransaction();
+//     this._litestore.updateRecord(
+//       {
+//         schema: DATAMODEL_SCHEMA,
+//         record: RECORD_ID
+//       },
+//       {
+//         rowMap: this._rowMap,
+//         columnMap: this._columnMap,
+//         valueMap: this._valueMap,
+//         change: change
+//       }
+//     );
+//   }
+// }
+
+export const SCHEMA_ID = 'datamodel';
+export const RECORD_ID = 'datamodel';
+export const DATAMODEL_SCHEMA = {
+  id: SCHEMA_ID,
+  fields: {
+    rowMap: Fields.Register<number[]>({ value: [] }),
+    columnMap: Fields.Register<number[]>({ value: [] }),
+    valueMap: Fields.Register<HashMap>({ value: { r0c0: '' } }),
+    change: Fields.Register<DataModel.ChangedArgs>({
+      value: { type: 'model-reset' }
+    })
+  }
+};
+
+export type HashMap = { [key: string]: string };
