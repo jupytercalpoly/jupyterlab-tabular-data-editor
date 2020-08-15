16bef3be2e2702b0d5bb8513944eb0caad407bb9
Logan McNichols <loganamcnichols@gmail.com>
2020-08-15 11:23:42 -0700

    Override of paintContent method added to PaintedGrid.

--- src/datagrid.ts
+++ src/datagrid.ts
@@ -1,3 +1,18 @@
 import { DataGrid } from 'tde-datagrid';
 
-export default class PaintedGrid extends DataGrid {}
+export default class PaintedGrid extends DataGrid {
+  /**
+   * @override paints on the ghost row and column as well after painting the other regions.
+   * Paint the grid content for the given dirty rect.
+   *
+   * The rect should be expressed in valid viewport coordinates.
+   *
+   * This is the primary paint entry point. The individual `_draw*`
+   * methods should not be invoked directly. This method dispatches
+   * to the drawing methods in the correct order.
+   */
+  protected paintContent(rx: number, ry: number, rw: number, rh: number): void {
+    // Paint in the background, rows, columns, and cells first.
+    super.paintContent(rx, ry, rw, rh);
+  }
+}
